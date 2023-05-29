9ac923531b5d3a319484a91cb1e0edb0e82abb88
Mathis Poncet-Crouzet <math712b@gmail.com>
2023-05-29 14:11:56 +0200

    WIP: add confirm password field and show error of password constraints

--- BlazorApp/Dto/UserRegistration.cs
+++ BlazorApp/Dto/UserRegistration.cs
@@ -5,12 +5,77 @@ namespace BlazorApp.Dto;
 
 public record UserRegistration()
 {
-    public string Password { get; set; } = String.Empty;
     [Required(ErrorMessage = "Le nom est obligatoire.")]
     [FirstLetterUppercase]
     public string Nom { get; set; } = String.Empty;
+
     [Required(ErrorMessage = "Le prénom est obligatoire.")]
     [FirstLetterUppercase]
     public string Prenom { get; set; } = String.Empty;
+
+    [Required(ErrorMessage = "Veuillez renseigner une date de naissance.")]
     public DateTime DateNaissance { get; set; } = DateTime.Now;
+    
+    [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+    [CustomValidation(typeof(UserRegistration), nameof(ValidatePasswordLength))]
+    [CustomValidation(typeof(UserRegistration), nameof(ValidatePasswordNonAlphaNumeric))]
+    [CustomValidation(typeof(UserRegistration), nameof(ValidatePasswordDigit))]
+    public string Password { get; set; } = String.Empty;
+    
+    [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+    [Compare(nameof(Password), ErrorMessage = "Les mots de passe ne correspondent pas.")]
+    public string ConfirmPassword { get; set; } = String.Empty;
+    
+    public int MinLengthPassword { get; init; } 
+    
+    public bool RequireNonAlphaNumeric { get; init; }
+    public bool RequireDigit { get; set; }
+
+    public static ValidationResult? ValidatePasswordLength(string password, ValidationContext context)
+    {
+        var instance = context.ObjectInstance as UserRegistration;
+        if (instance == null)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (password.Length < instance.MinLengthPassword)
+        {
+            return new ValidationResult($"Le mot de passe doit contenir au moins {instance.MinLengthPassword} caractères.");
+        }
+
+        return ValidationResult.Success;
+    }
+
+    public static ValidationResult? ValidatePasswordNonAlphaNumeric(string password, ValidationContext context)
+    {
+        var instance = context.ObjectInstance as UserRegistration;
+        if (instance == null)
+        {
+            return ValidationResult.Success;
+        }
+        
+        if (instance.RequireNonAlphaNumeric && !password.Any(char.IsDigit))
+        {
+            return new ValidationResult("Le mot de passe doit contenir au moins un caractère numérique.");
+        }
+        
+        return ValidationResult.Success;
+    }
+    
+    public static ValidationResult? ValidatePasswordDigit(string password, ValidationContext context)
+    {
+        var instance = context.ObjectInstance as UserRegistration;
+        if (instance == null)
+        {
+            return ValidationResult.Success;
+        }
+        
+        if (instance.RequireDigit && !password.Any(c => char.IsPunctuation(c) || char.IsSymbol(c)))
+        {
+            return new ValidationResult("Le mot de passe doit contenir au moins un caractère spécial.");
+        }
+        
+        return ValidationResult.Success;
+    }
 }
