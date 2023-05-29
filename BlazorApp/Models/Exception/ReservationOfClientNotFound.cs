fa487f5ec724ae49e9218fdb60248b2869f3125e
Mathis Poncet-Crouzet <math712b@gmail.com>
2023-05-29 17:53:29 +0200

    feat: show the reservations of logged user

---
+++ BlazorApp/Models/Exception/ReservationOfClientNotFound.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorApp.Models.Exception;
+
+public class ReservationOfClientNotFound : System.Exception
+{
+    public ReservationOfClientNotFound(): base("Vos réservations n'ont pas été trouvées") { }
+}
--- BlazorApp/Services/ReservationService.cs
+++ BlazorApp/Services/ReservationService.cs
@@ -1,5 +1,6 @@
 ﻿using BlazorApp.Dto;
 using BlazorApp.Models.Domain;
+using BlazorApp.Models.Exception;
 using BlazorApp.Repository;
 
 namespace BlazorApp.Services;
@@ -7,15 +8,34 @@ namespace BlazorApp.Services;
 public class ReservationService
 {
     private readonly VehiculeRepository _vehiculeRepository;
+    private readonly ReservationRepository _reservationRepository;
 
-    public ReservationService(VehiculeRepository vehiculeRepository)
+    public ReservationService(VehiculeRepository vehiculeRepository, ReservationRepository reservationRepository)
     {
         _vehiculeRepository = vehiculeRepository;
+        _reservationRepository = reservationRepository;
     }
 
     public void Reserver(Vehicule vehicule, Client client, PlageReservation plageReservation)
     {
         vehicule.Reserver(client, plageReservation);
         _vehiculeRepository.Update();
+    }
+    
+    /// <summary>
+    /// Récupère les réservations d'un client
+    /// </summary>
+    /// <param name="client">Le client à chercher dans les réservations à son nom</param>
+    /// <returns></returns>
+    /// <exception cref="ReservationOfClientNotFound">Si le client n'a aucunes réservations</exception>
+    public List<Reservation> Reservations(Client client)
+    {
+        var reservations = _reservationRepository.Reservations(client);
+        if (reservations.Count == 0)
+        {
+            throw new ReservationOfClientNotFound();
+        }
+
+        return reservations;
     }
 }
